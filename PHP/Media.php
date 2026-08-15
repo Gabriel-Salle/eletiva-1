d9916d0b035ed09b559494aef3efe2f632046c4e
Salle <gabrielsalle20@gmail.com>
2026-08-14 23:38:43 -0300

    subtracao e multiplicacao

---
+++ PHP/Media.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Soma</title>
+</head>
+<body>
+    <form method="post" action="RespostaMedia.php">
+        <label for="nota1">Informe a pimeira nota</label>
+        <input type="number" id="nota1" name="nota1">
+        <label for="nota2">Informe a segunda nota</label>
+        <input type="number" id="nota2" name="nota2">
+        <label for="nota3">Informe a terceira nota</label>
+        <input type="number" id="nota3" name="nota3">
+        <button type="submit">enviar</button>
+    </form>
+</body>
+</html>
---
+++ PHP/subtracao.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>subtração</title>
+</head>
+<body>
+    <form method="post" action="RespostaSubtracao">
+        <label for="valor1">Informe o primeiro valor</label>
+        <input type="number" id="valor1" name="valor1">
+        <label for="valor2">Informe o segundo valor</label>
+        <input type="number" id="valor2" name="valor2">
+        <button type="submit">enviar</button>
+    </form>
+</body>
+</html>
